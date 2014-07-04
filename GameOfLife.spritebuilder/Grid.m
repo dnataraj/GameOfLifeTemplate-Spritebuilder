51250e5485b66b5ff44f1ba1c70e69a470e39825
Deepak Natarajan <dnataraj@trilobytesystems.com>
2014-07-04 11:00:28 +0300

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -113,9 +113,12 @@ static const int GRID_COLUMNS = 10;
             Creature *currentCreature = _gridArray[i][j];
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
-                numAlive += 1;
+                //numAlive += 1;
             } else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = NO;
+            }
+            if (currentCreature.isAlive) {
+                numAlive += 1;
             }
         }
     }
